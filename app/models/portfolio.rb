bd28994e7dfead37dcb64f33d65d29fc85e062c8
April Campos <aprilcamille.campos@gmail.com>
2022-10-07 21:12:30 +0800

    implemented image dafeults

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -6,4 +6,14 @@ class Portfolio < ApplicationRecord
     end
 
 		scope :ruby_on_rails_portfolio_items, -> { where(subtitle: "Ruby on Rails") }
+
+		after_initialize :set_defaults
+
+		def set_defaults
+			self.main_image ||= "https://pixy.org/ph/06139e/7a7a7a/frame/crosshairs/350x200.png"
+			self.thumb_image ||= "https://tse4.mm.bing.net/th?id=OIP.4L0hr_SUbeH1q-I2KrFg0QAAAA&pid=Api&P=0"
+		end
+
+		
+    
 end
