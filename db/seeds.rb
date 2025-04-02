4e8cb9c42ba9f541ea19ca5376035c063e7cbc4b
April Camille Campos <aprilcamille.campos@gmail.com>
2025-04-02 12:18:48 +0800

    Implemented show page styles for portfolio layout

--- db/seeds.rb
+++ db/seeds.rb
@@ -42,7 +42,7 @@ puts "5 skills created"
          consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui 
          dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora 
          incidunt ut labore et dolore magnam aliquam quaerat voluptatem.",
-        main_image: "https://pixy.org/ph/06139e/7a7a7a/frame/crosshairs/350x200.png",
+        main_image: "https://pixy.org/ph/06139e/7a7a7a/frame/crosshairs/600x400.png",
         thumb_image: "https://tse4.mm.bing.net/th?id=OIP.4L0hr_SUbeH1q-I2KrFg0QAAAA&pid=Api&P=0"
     )
 end
@@ -57,7 +57,7 @@ end
          consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui 
          dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora 
          incidunt ut labore et dolore magnam aliquam quaerat voluptatem.",
-        main_image: "https://pixy.org/ph/06139e/7a7a7a/frame/crosshairs/350x200.png",
+        main_image: "https://pixy.org/ph/06139e/7a7a7a/frame/crosshairs/600x400.png",
         thumb_image: "https://tse4.mm.bing.net/th?id=OIP.4L0hr_SUbeH1q-I2KrFg0QAAAA&pid=Api&P=0"
     )
 end
