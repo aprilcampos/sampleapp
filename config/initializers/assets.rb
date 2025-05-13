d94e379c987e74d44f7dee46ecc337e79111c5a6
April Camille Campos <aprilcamille.campos@gmail.com>
2025-05-13 17:39:33 +0800

    uncomment css file

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,4 +13,4 @@ Rails.application.config.assets.version = "1.0"
 
 Rails.application.config.assets.paths << Rails.root.join("app", "assets", "fonts")
 
-# Rails.application.config.assets.precompile += %w( application.css )
+Rails.application.config.assets.precompile += %w( application.css )
