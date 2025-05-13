9e8858a1c21b415f5c79186e0081e571532a28da
April Camille Campos <aprilcamille.campos@gmail.com>
2025-05-13 13:13:29 +0800

    updaet render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,9 +3,8 @@
 set -o errexit
 
 bundle install
-bundle exec rails assets:clobber
-bundle exec rails assets:precompile RAILS_ENV=production
-cat public/assets/importmap.json 
+bundle exec rails assets:precompile
+bundle exec rails assets:clean
 
 # If you're using a Free instance type, you need to
 # perform database migrations in the build command.
