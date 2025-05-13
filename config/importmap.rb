0f26313ed528e1f10f256fe221259f40ea47bc5c
April Camille Campos <aprilcamille.campos@gmail.com>
2025-05-13 14:58:31 +0800

    fix pins in importmap

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,8 +5,7 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 
-pin "controllers", to: "controllers/index.js"
-pin "controllers/sortable_controller", to: "controllers/sortable_controller.js", preload: true
+pin "controllers/application", to: "controllers/application.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin_all_from "app/javascript/channels", under: "channels"
 pin "@rails/actioncable", to: "actioncable.esm.js"
