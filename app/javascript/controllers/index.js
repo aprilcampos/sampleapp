14d13d807a1dd40b02465bed8cce0b251fbd8e35
April Camille Campos <aprilcamille.campos@gmail.com>
2025-05-13 09:13:08 +0800

    remove hamburger controller

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,8 +1,5 @@
 import { application } from "./application"
 
-import HamburgerController from "./hamburger_controller"
-application.register("hamburger", HamburgerController)
-
 import SortableController from "./sortable_controller"
 application.register("sortable", SortableController)
 
